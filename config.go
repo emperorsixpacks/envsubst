cd1ae9fe02895dfd06624e79da1b6901a82bde33
emperorsixpacks <andrewdvd10@gmail.com>
2025-05-13 19:12:21 +0100

    Revert "fix: config parsing error"
    This reverts commit 9dd23ccb91fe4a95b2127958ba9b3965d8499c67.

--- config.go
+++ config.go
@@ -2,14 +2,16 @@ package envsubt
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"strings"
 
 	"gopkg.in/yaml.v3"
 )
 
-func Unmarshal(in []byte, o any) error {
+func Unmarshal(in []byte, o interface{}) error {
 	ymlBytes, err := unmarshal(in)
+	fmt.Println(ymlBytes)
 	if err != nil {
 		return err
 	}
@@ -51,12 +53,12 @@ func ymltoMap(file []byte) (any, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return config, nil
 }
 
-func resolveConfig(config *any) error {
-	MapConfig, err := validMapping(*config)
+// TODO why don't we just pass the variable around without the pointer
+func resolveConfig(config any) error {
+	MapConfig, err := validMapping(config)
 	if err != nil {
 		return err
 	}
@@ -69,16 +71,11 @@ func resolveConfig(config *any) error {
 	return nil
 }
 
-// TODO accept interface{} val of interface{} type, currently only works with str
+// TODO accept any val of any type, currently only works with str
 func resolveConfigVars(config any) (any, error) {
 	MapConfig, err := validMapping(config)
 	if err != nil {
-		comma, ok := config.(any)
-		if !ok {
-			return nil, errors.New("Invalid field type")
-		}
-		return resolvePlaceHolder(comma), nil
-
+		return nil, err
 	}
 	for k, v := range MapConfig {
 		if value, ok := v.(string); ok {
@@ -92,15 +89,11 @@ func resolveConfigVars(config any) (any, error) {
 	return config, nil // MapConfig is a reference to config
 }
 
-func resolvePlaceHolder(value any) any {
-	strValue, ok := value.(string)
-	if !ok {
-		return value
-	}
-	if strings.Contains(strValue, "${") {
-		last_index := len(strValue) - 1
+func resolvePlaceHolder(value string) string {
+	if strings.Contains(value, "${") {
+		last_index := len(value) - 1
 		first_index := 2
-		env_value := strValue[first_index:last_index]
+		env_value := value[first_index:last_index]
 		return os.Getenv(env_value)
 	}
 	return value
